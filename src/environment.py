a3069f7d88532c38f48716a798404164606b4a58
Eros-Pinzani <eros.pinzani@edu.unifi.it>
2025-10-20 16:23:29 +0200

    Iniziato a lavorare su environment

--- src/environment.py
+++ src/environment.py
@@ -1,18 +1,33 @@
-# Classe che contiene gli ostacoli, i confini e funzioni di utilità geometriche
+# Classe che contiene gli ostacoli (gestiti con Shapely), i confini, le intersezioni e funzioni di utilità geometriche
 
-from shapely.geometry import Polygon, box
+from shapely.geometry import Polygon, box, LineString, Point
+from shapely.ops import unary_union
 import matplotlib.pyplot as plt
 
 class Environment:
-    """Placeholder per la gestione dell'ambiente.
-    In futuro qui verranno implementati:
-      - Ostacoli (tramite shapely)
-      - Limiti del mondo
-      - Collision checking
-    Per ora serve solo come struttura vuota."""
-
     def __init__(self):
+        self.obstacles = [] # Lista di ostacoli (oggetti Shapely)
+        self.bounds = None # Confini dell’ambiente (oggetto Shapely)
+
+    def set_bounds(self, xmin, ymin, xmax, ymax):
+        """Imposta i confini dell’ambiente come un rettangolo."""
+        self.bounds = box(xmin, ymin, xmax, ymax)
+
+    def add_obstacle(self, poly):
+        """Aggiunge un ostacolo all’ambiente."""
+        self.obstacles.append(poly)
+
+    def add_rectangle(self, xmin, ymin, xmax, ymax):
+        """Aggiunge un ostacolo rettangolare all’ambiente."""
+        self.obstacles.append(box(xmin, ymin, xmax, ymax))
+
+    def clear(self):
+        self.obstacles = []
+        self.bounds = None
+
+    def obstacles_union(self):
         pass
+        #TODO
 
     def plot(self, ax):
         """Placeholder per il disegno dell’ambiente (attualmente vuoto)."""
